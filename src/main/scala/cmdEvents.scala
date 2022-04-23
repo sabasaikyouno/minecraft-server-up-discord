d843584a91768472dc17a0ae80295a651dca7ced
sabasaikyouno <0852kuro@gmail.com>
2022-04-24 04:40:30 +0900

    configを取得できるコマンドを追加

--- src/main/scala/cmdEvents.scala
+++ src/main/scala/cmdEvents.scala
@@ -1,7 +1,8 @@
 import ackcord.{CacheSnapshot, DiscordClient}
 import ackcord.data.{Message, TextChannelId}
-import ackcord.requests.{CreateMessage, CreateMessageData}
+import ackcord.requests.{CreateMessage, CreateMessageData, CreateMessageFile}
 import Backup._
+import java.nio.file.Path
 
 import scala.sys.process.Process
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -13,11 +14,12 @@ object cmdEvents {
 
     message.content match {
       case "!start-server" => startServer()
-      case "!address" => sendMsg(sys.env("Minecraft_Address"))
-      case "!mod" => sendMsg("https://1drv.ms/u/s!Andx-P21NVAYi5o8kYlDGXFscGvYDQ")
-      case "!help" => sendMsg(createHelp)
-      case "!github" => sendMsg("https://github.com/sabasaikyouno/minecraft-server-up-discord")
-      case "!backup" => backup()
+      case "!address"     => sendMsg(sys.env("Minecraft_Address"))
+      case "!mod"         => sendMsg("https://1drv.ms/u/s!Andx-P21NVAYi5o8kYlDGXFscGvYDQ")
+      case "!help"        => sendMsg(createHelp)
+      case "!github"      => sendMsg("https://github.com/sabasaikyouno/minecraft-server-up-discord")
+      case "!backup"      => backup()
+      case msg if msg.take(7) == "!config" => cmdConfig(msg)
     }
   }
 
@@ -42,11 +44,34 @@ object cmdEvents {
       ("!address", "マイクラのサーバーアドレス"),
       ("!mod", "mod一覧ファイル"),
       ("!github", "discord-botのsource"),
-      ("!backup", "マイクラのbackupを取る")
+      ("!backup", "マイクラのbackupを取る"),
+      ("!config", "modのconfig一覧")
+    ))
+
+  def cmdConfig(msg: String)(implicit cacheSnapshot: CacheSnapshot, client: DiscordClient, channelId: TextChannelId) =
+    msg.drop(8) match {
+      case "mineall" => sendFile(Path.of("C:\\Users\\kurotan\\AppData\\Roaming\\.mod3server1.12.2\\config\\net.minecraft.scalar.mineall.mod_mineallsmp.cfg"))
+      case "cutall" => sendFile(Path.of("C:\\Users\\kurotan\\AppData\\Roaming\\.mod3server1.12.2\\config\\net.minecraft.scalar.cutall.mod_cutallsmp.cfg"))
+      case "" => sendMsg(createHelpConfig)
+    }
+
+  def sendFile(path: Path)(implicit c: CacheSnapshot, client: DiscordClient, channelId: TextChannelId) =
+    client.requestsHelper.run(
+      CreateMessage(
+        channelId,
+        CreateMessageData(files = Seq(CreateMessageFile.FromPath(path)))
+      )
+    ).map(_ => ())
+
+  def createHelpConfig =
+    fmtHelp(Seq(
+      ("使い方", "!config modの名前"),
+      ("mineall", "mineallのconfig"),
+      ("cutall", "ctuallのconfig")
     ))
 
   private def fmtHelp(seq: Seq[(String, String)]) = {
-    val maxWidth = seq.maxBy(_._1)._1.length + 2
+    val maxWidth = seq.maxBy(_._1.length)._1.length + 2
 
     seq.map { case (cmd, memo) =>
       cmd + " " * (maxWidth - cmd.length) + memo
