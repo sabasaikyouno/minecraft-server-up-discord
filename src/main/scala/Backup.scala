f5b14cfdc931fcd927c757088e37042256c044d5
sabasaikyouno <0852kuro@gmail.com>
2022-04-27 02:58:11 +0900

    重複したバックアップを取らないようにした。 バックアップを10個以上取らないようにした。

--- src/main/scala/Backup.scala
+++ src/main/scala/Backup.scala
@@ -1,19 +1,51 @@
 import java.io.File
 import java.util.Date
 
+import akka.actor.{ActorSystem, Props}
+import scala.concurrent.duration._
 import org.apache.commons.io.FileUtils._
+import java.nio.file.Files.delete
 
 object Backup {
 
-  def backup() = {
-    val worldFilePath = new File("C:\\Users\\kurotan\\AppData\\Roaming\\.mod3server1.12.2\\world")
-    val folder = "world-" + new Date().toString.collect{ case ':' => '-'; case s => s}
-    val backupFilePath = new File("C:\\Users\\kurotan\\AppData\\Roaming\\.mod3server1.12.2\\backup\\" + folder)
+  private val worldFilePath = "C:\\Users\\kurotan\\AppData\\Roaming\\.mod3server1.12.2\\world"
+  private val backupFilePath = "C:\\Users\\kurotan\\AppData\\Roaming\\.mod3server1.12.2\\backup"
 
-    copyDirectory(worldFilePath, backupFilePath)
+  def backup() = {
+    val worldFile = new File(worldFilePath)
+    val folder = "\\world-" + new Date().toString.collect{ case ':' => '-'; case s => s}
+    val backupFile = new File(backupFilePath + folder)
+
+    if (canBackup()) {
+      copyDirectory(worldFile, backupFile)
+      deleteBackup(10)
+    }
   }
 
   def startAutoBackup() = {
-    AkkaScheduler
+    import actorSystem.dispatcher
+
+    val actorSystem = ActorSystem("BackupActor")
+    val backupActor = actorSystem.actorOf(Props(new BackupActor))
+
+    actorSystem.scheduler.schedule(0.milliseconds,4.hours, backupActor, "Backup")
+  }
+
+  // 重複したbackupを取らないようにする
+  def canBackup() = {
+    val worldFile = new File(worldFilePath)
+    val backupFiles = new File(backupFilePath).listFiles()
+
+    if (backupFiles.nonEmpty)
+      worldFile.lastModified() != backupFiles.max.lastModified()
+    else
+      true
+  }
+
+  // backupファイル数がfileMax以上になったら超えた数を消す
+  def deleteBackup(fileMax: Int) = {
+    val backupFiles = new File(backupFilePath).listFiles().sorted.reverse
+
+    backupFiles.drop(fileMax).foreach(deleteDirectory)
   }
 }
--- src/main/scala/BackupActor.scala
+++ src/main/scala/BackupActor.scala
@@ -1,15 +1,5 @@
-import akka.actor.{Actor, ActorSystem, Props}
-import scala.concurrent.duration._
+import akka.actor.Actor
 import Backup._
-
-object AkkaScheduler {
-  import actorSystem.dispatcher
-
-  val actorSystem = ActorSystem("BackupActor")
-  val backupActor = actorSystem.actorOf(Props(new BackupActor))
-
-  actorSystem.scheduler.schedule(0.milliseconds,4.hours, backupActor, "Backup")
-}
 
 class BackupActor extends Actor {
 
